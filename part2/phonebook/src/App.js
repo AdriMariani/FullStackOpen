6065678a66d7e934e4f50ce4527797adec63546e
Adriano Mariani <adrianomariani25@gmail.com>
2020-07-31 23:06:37 -0400

    2.6-2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Phonebook from './components/Phonebook'
 
-function App() {
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ filter, setFilter ] = useState('')
+
+  const handleInputChange = (event) => {
+    event.target.name === 'name' ? 
+      setNewName(event.target.value) : 
+      setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const validateForm = () => {
+    const pattern = /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/;
+    let message = 'Error: \n'
+    let isValid = true
+
+    if(persons.some(person => person.name === newName)) {
+      message += `${newName} is already added to phonebook.\n`
+      isValid = false
+    }
+    if (!newNumber.match(pattern)) {
+      message += `${newNumber} is not a valid phone number.`
+      isValid = false
+    }
+
+    if (!isValid)
+      alert(message)
+
+    return isValid    
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    if(validateForm()) {
+      const newPerson = {
+        name: newName,
+        number: newNumber
+      }
+      setPersons(persons.concat(newPerson))
+      setNewName('')
+      setNewNumber('')
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        filter shown with <input value={filter} onChange={handleFilterChange}></input>
+      </form>
+      <h3>Add New Entry</h3>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input name="name" value={newName} onChange={handleInputChange}/>
+        </div>
+        <div>
+          number: <input name="number" value={newNumber} onChange={handleInputChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {filter === '' ?
+          <Phonebook persons={persons}/> :
+          <Phonebook 
+            persons={persons.filter(person => {
+              return person.name.toLowerCase().startsWith(filter.toLowerCase())
+            })}
+          /> 
+        }
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
