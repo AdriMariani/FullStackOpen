570aaaf8afbbf9d168eef180f22657f37bacb372
Adriano Mariani <adrianomariani25@gmail.com>
2020-08-20 22:52:18 -0400

    4.5

--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -4,7 +4,16 @@ const totalLikes = (blogs) => {
   return blogs.reduce( (sum, blog) => sum + blog.likes, 0)
 }
 
+const favouriteBlog = (blogs) => {
+  return blogs.length === 0 
+    ? null 
+    : blogs.reduce( (fav, blog) => {
+        return blog.likes > fav.likes ? blog : fav
+      })
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlog
 }
