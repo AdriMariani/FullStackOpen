b2d87b1a832b186d77c95ef70c154fc2676e1033
Adriano Mariani <adrianomariani25@gmail.com>
2020-08-30 22:10:37 -0400

    5.3

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -3,12 +3,16 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import LoginForm from './components/LoginForm'
+import BlogForm from './components/BlogForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [blogTitle, setBlogTitle] = useState('')
+  const [blogUrl, setBlogUrl] = useState('')
+  const [blogAuthor, setBlogAuthor] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -21,6 +25,7 @@ const App = () => {
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
+      blogService.setToken(user.token)
     }
   }, [])
 
@@ -34,6 +39,7 @@ const App = () => {
           'loggedBlogAppUser', JSON.stringify(userData)
         )
         setUser(userData)
+        blogService.setToken(userData.token)
         setUsername('')
         setPassword('')
       })
@@ -43,6 +49,21 @@ const App = () => {
   const handleLogout = () => {
     window.localStorage.removeItem('loggedBlogAppUser')
     setUser(null)
+  }
+
+  const addBlog = async event => {
+    event.preventDefault()
+
+    const newBlog = await blogService.createBlog({
+      title: blogTitle,
+      author: blogAuthor,
+      url: blogUrl
+    })
+
+    setBlogs(blogs.concat(newBlog))
+    setBlogTitle('')
+    setBlogAuthor('')
+    setBlogUrl('')
   }
 
   return (
@@ -63,6 +84,15 @@ const App = () => {
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
+        <BlogForm 
+          blogTitle={blogTitle}
+          setBlogTitle={setBlogTitle}
+          blogAuthor={blogAuthor}
+          setBlogAuthor={setBlogAuthor}
+          blogUrl={blogUrl}
+          setBlogUrl={setBlogUrl}
+          addBlog={addBlog}
+        />
       </div> 
   )
 }
---
+++ part5/bloglist-frontend/src/components/BlogForm.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const BlogForm = props => 
+  <div>
+    <h2>Create New Blog</h2>
+    <form onSubmit={props.addBlog}>
+      <div>
+        Title:
+        <input
+          value={props.blogTitle}
+          onChange={event => props.setBlogTitle(event.target.value)}
+        />
+      </div>
+      <div>
+        Author:
+        <input
+          value={props.blogAuthor}
+          onChange={event => props.setBlogAuthor(event.target.value)}
+        />
+      </div>
+      <div>
+        Url:
+        <input
+          value={props.blogUrl}
+          onChange={event => props.setBlogUrl(event.target.value)}
+        />
+      </div>
+      <button type="submit">save</button>
+    </form>
+  </div> 
+
+export default BlogForm
