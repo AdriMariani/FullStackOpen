28400f3189ec70bbea13cf0fe0d732162e74d023
Adriano Mariani <adrianomariani25@gmail.com>
2020-08-30 21:32:56 -0400

    5.1

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
+import LoginForm from './components/LoginForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,13 +16,35 @@ const App = () => {
     )  
   }, [])
 
+  const handleLogin = event => {
+    event.preventDefault()
+
+    loginService
+      .login({ username, password })
+      .then(userData => {
+        setUser(userData)
+        setUsername('')
+        setPassword('')
+      })
+      .catch(err => alert('Invalid username or password'))
+  }
+
   return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
+    user === null ?
+      <LoginForm 
+        username={username} 
+        password={password} 
+        setUsername={setUsername}
+        setPassword={setPassword}
+        handleLogin={handleLogin}
+      /> :
+      <div>
+        <h2>Blogs</h2>
+        <p>{user.name} is currently logged in.</p>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div> 
   )
 }
 
