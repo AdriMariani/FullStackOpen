072a58a071198b4cdd82869286156d40b2385b9f
Adriano Mariani <adrianomariani25@gmail.com>
2020-09-04 21:00:03 -0400

    5.19

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,11 +1,13 @@
 describe('Blog app', function() {
+  const user = {
+    username: 'jteller',
+    password: 'samcro',
+    name: 'Jax Teller'
+  }
+
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
-    cy.request('POST', 'http://localhost:3001/api/users', {
-      username: 'jteller',
-      password: 'samcro',
-      name: 'Jax Teller'
-    })
+    cy.request('POST', 'http://localhost:3001/api/users', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -16,15 +18,15 @@ describe('Blog app', function() {
 
   describe('Login',function() {
     it('succeeds with correct credentials', function() {
-      cy.get('#username').type('jteller')
-      cy.get('#password').type('samcro')
+      cy.get('#username').type(user.username)
+      cy.get('#password').type(user.password)
       cy.get('#loginButton').click()
 
-      cy.contains('Jax Teller is currently logged in.')
+      cy.contains(`${user.name} is currently logged in.`)
     })
 
     it('fails with wrong credentials', function() {
-      cy.get('#username').type('jteller')
+      cy.get('#username').type(user.username)
       cy.get('#password').type('incorrect')
       cy.get('#loginButton').click()
 
@@ -33,8 +35,31 @@ describe('Blog app', function() {
         .and('have.css', 'color', 'rgb(255, 0, 0)')
         .and('have.css', 'border-style', 'solid')
 
-      cy.get('html').should('not.contain', 'Jax Teller logged in')
+      cy.get('html').should('not.contain', `${user.name} is currently logged in.`)
       cy.contains('Login to Application')
+    })
+  })
+
+  describe('When logged in', function() {
+    beforeEach(function() {
+      cy.login({ username: user.username, password: user.password })
+    })
+
+    it('A blog can be created', function() {
+      const blog = {
+        title: 'some blog',
+        author: 'tester',
+        url: 'http://www.ablog.ca'
+      }
+
+      cy.contains('New Blog').click()
+      cy.get('#title').type(blog.title)
+      cy.get('#author').type(blog.author)
+      cy.get('#url').type(blog.url)
+      cy.contains('save').click()
+
+      cy.get('.titleAndAuthor')
+        .should('contain', `${blog.title} by ${blog.author}`)
     })
   })
 })
