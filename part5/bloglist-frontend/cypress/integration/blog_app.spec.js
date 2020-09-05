b1e8450ebb1177c7ef1a662c9d78422044b2d114
Adriano Mariani <adrianomariani25@gmail.com>
2020-09-04 22:39:31 -0400

    5.21

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,13 +1,19 @@
 describe('Blog app', function() {
-  const user = {
+  const userOne = {
     username: 'jteller',
     password: 'samcro',
     name: 'Jax Teller'
   }
+  const userTwo = {
+    username: 'chibs',
+    password: 'sambel',
+    name: 'Filip Telford'
+  }
 
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
-    cy.request('POST', 'http://localhost:3001/api/users', user)
+    cy.request('POST', 'http://localhost:3001/api/users', userOne)
+    cy.request('POST', 'http://localhost:3001/api/users', userTwo)
     cy.visit('http://localhost:3000')
   })
 
@@ -18,15 +24,15 @@ describe('Blog app', function() {
 
   describe('Login',function() {
     it('succeeds with correct credentials', function() {
-      cy.get('#username').type(user.username)
-      cy.get('#password').type(user.password)
+      cy.get('#username').type(userOne.username)
+      cy.get('#password').type(userOne.password)
       cy.get('#loginButton').click()
 
-      cy.contains(`${user.name} is currently logged in.`)
+      cy.contains(`${userOne.name} is currently logged in.`)
     })
 
     it('fails with wrong credentials', function() {
-      cy.get('#username').type(user.username)
+      cy.get('#username').type(userOne.username)
       cy.get('#password').type('incorrect')
       cy.get('#loginButton').click()
 
@@ -35,14 +41,14 @@ describe('Blog app', function() {
         .and('have.css', 'color', 'rgb(255, 0, 0)')
         .and('have.css', 'border-style', 'solid')
 
-      cy.get('html').should('not.contain', `${user.name} is currently logged in.`)
+      cy.get('html').should('not.contain', `${userOne.name} is currently logged in.`)
       cy.contains('Login to Application')
     })
   })
 
   describe('When logged in', function() {
     beforeEach(function() {
-      cy.login({ username: user.username, password: user.password })
+      cy.login({ username: userOne.username, password: userOne.password })
     })
 
     it('A blog can be created', function() {
@@ -87,7 +93,31 @@ describe('Blog app', function() {
         cy.get('@blog').contains('View').click()
         cy.get('@blog').contains('Like').click()
 
-        cy.get('.likes').contains(`${blogTwo.likes + 1}`)
+        cy.get('.likes').should('contain', `${blogTwo.likes + 1}`)
+      })
+
+      it('can delete a blog', function() {
+        cy.contains(`${blogOne.title} by ${blogOne.author}`).parent().as('blog')
+
+        cy.get('@blog').contains('View').click()
+        cy.get('@blog').contains('Delete').click()
+
+        cy.get('.success')
+          .should('contain', `Successfully deleted ${blogOne.title} by ${blogOne.author}`)
+          .and('have.css', 'color', 'rgb(0, 128, 0)')
+          .and('have.css', 'border-style', 'solid')
+
+        cy.get('html', { timeout: 6000 }) //must wait for notification to disappear
+          .should('not.contain', `${blogOne.title} by ${blogOne.author}`)
+      })
+
+      it('a different user cannot delete a blog not created by them', function() {
+        cy.login({ username: userTwo.username, password: userTwo.password })
+
+        cy.contains(`${blogOne.title} by ${blogOne.author}`).parent().as('blog')
+
+        cy.get('@blog').contains('View').click()
+        cy.get('@blog').should('not.contain', 'Delete')
       })
     })
   })
