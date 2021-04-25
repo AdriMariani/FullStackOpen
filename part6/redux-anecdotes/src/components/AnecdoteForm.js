eefdfccb808a56441491bde86d8af4df4a893476
Adriano Mariani <adriano.mariani@ryerson.ca>
2021-04-25 15:39:46 -0400

    6.20 - use connect in Filter and AnecdoteForm

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,16 +1,13 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
-import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = props => {
-  const dispatch = useDispatch()
-
   const addAnecdote = e => {
     e.preventDefault()
     const content = e.target.newAnecdote.value
     e.target.newAnecdote.value = ''
-    dispatch(createAnecdote(content))
+    props.createAnecdote(content)
   }
 
   return (
@@ -24,4 +21,35 @@ const AnecdoteForm = props => {
   )
 }
 
-export default AnecdoteForm
+export default connect(
+  null,
+  { createAnecdote }
+)(AnecdoteForm)
+
+// Code using useDispatch hook
+// import React from 'react'
+// import { useDispatch } from 'react-redux'
+// import { createAnecdote } from '../reducers/anecdoteReducer'
+
+// const AnecdoteForm = props => {
+//   const dispatch = useDispatch()
+
+//   const addAnecdote = e => {
+//     e.preventDefault()
+//     const content = e.target.newAnecdote.value
+//     e.target.newAnecdote.value = ''
+//     dispatch(createAnecdote(content))
+//   }
+
+//   return (
+//     <div>
+//       <h2>create new</h2>
+//       <form onSubmit={addAnecdote}>
+//         <div><input name='newAnecdote' /></div>
+//         <button type='submit'>create</button>
+//       </form>
+//     </div>
+//   )
+// }
+
+// export default AnecdoteForm
--- part6/redux-anecdotes/src/components/Filter.js
+++ part6/redux-anecdotes/src/components/Filter.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
 import { setFilter } from '../reducers/filterReducer'
 
-const Filter = () => {
-  const dispatch = useDispatch()
-
+const Filter = props => {
   const handleChange = (event) => {
-    dispatch(setFilter(event.target.value))
+    props.setFilter(event.target.value)
   }
 
   const style = {
@@ -20,4 +18,32 @@ const Filter = () => {
   )
 }
 
-export default Filter
+export default connect(
+  null,
+  { setFilter }
+)(Filter)
+
+// Code using useDispatch hook
+// import React from 'react'
+// import { useDispatch } from 'react-redux'
+// import { setFilter } from '../reducers/filterReducer'
+
+// const Filter = () => {
+//   const dispatch = useDispatch()
+
+//   const handleChange = (event) => {
+//     dispatch(setFilter(event.target.value))
+//   }
+
+//   const style = {
+//     marginBottom: 10
+//   }
+
+//   return (
+//     <div style={style}>
+//       Filter <input onChange={handleChange} />
+//     </div>
+//   )
+// }
+
+// export default Filter
