63ca4f3cac8eda6fea7f5dcf71d2cd745e10f55d
Adriano Mariani <adriano.mariani@ryerson.ca>
2021-08-07 16:37:02 -0400

    Added use of fragments in queries

--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -1,71 +1,77 @@
 import { gql } from '@apollo/client'
 
-export const ALL_AUTHORS = gql`
-query {
-  allAuthors {
+const BOOK_DETAILS = gql`
+  fragment BookDetails on Book {
+    id
+    title
+    published
+    author {
+      name
+      born
+      bookCount
+    }
+    genres
+  }
+`
+
+const AUTHOR_DETAILS = gql`
+  fragment AuthorDetails on Author {
     id
     name
     born
     bookCount
   }
-}
+`
+
+export const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      ...AuthorDetails
+    }
+  }
+  ${AUTHOR_DETAILS}
 `
 
 export const ALL_BOOKS =gql`
-query allBooks($genre: String, $author: String) {
-  allBooks(genre: $genre, author: $author) {
-    id
-    title
-    published
-    author {
-      name
-      born
-      bookCount
+  query allBooks($genre: String, $author: String) {
+    allBooks(genre: $genre, author: $author) {
+      ...BookDetails
     }
-    genres
   }
-}
+  ${BOOK_DETAILS}
 `
 
 export const ADD_BOOK = gql`
-mutation newBook($title: String!, $author: String!, $published: Int!, $genres: [String!]!) {
-  addBook(title: $title, author: $author, published: $published, genres: $genres) {
-    title
-    author {
-      name
-      born
-      bookCount
+  mutation newBook($title: String!, $author: String!, $published: Int!, $genres: [String!]!) {
+    addBook(title: $title, author: $author, published: $published, genres: $genres) {
+      ...BookDetails
     }
-    published
-    genres
   }
-}
+  ${BOOK_DETAILS}
 `
 
 export const EDIT_AUTHOR = gql`
-mutation changeYear($author: String!, $born: Int!) {
-  editAuthor(name: $author, setBornTo: $born) {
-    id
-    name
-    born
-    bookCount
+  mutation changeYear($author: String!, $born: Int!) {
+    editAuthor(name: $author, setBornTo: $born) {
+      ...AuthorDetails
+    }
   }
-}
+  ${AUTHOR_DETAILS}
 `
 
 export const LOGIN = gql`
-mutation login($username: String!, $password: String!) {
-  login(username: $username, password: $password)  {
-    value
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password)  {
+      value
+    }
   }
-}
 `
 
 export const ME = gql`
-query {
-  me {
-    username
-    favouriteGenre
+  query {
+    me {
+      username
+      favouriteGenre
+    }
   }
-}
 `
