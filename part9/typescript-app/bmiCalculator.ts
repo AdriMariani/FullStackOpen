b50804f7735a98d1c510bc3944611bab4ff56290
Adriano Mariani <adriano.mariani@ryerson.ca>
2021-08-23 22:13:27 -0400

    9.5 - add bmi endpoint

--- part9/typescript-app/bmiCalculator.ts
+++ part9/typescript-app/bmiCalculator.ts
@@ -1,21 +1,21 @@
-interface BmiValues {
-    height: number,
-    weight: number
-}
+// interface BmiValues {
+//     height: number,
+//     weight: number
+// }
 
-function parseArguments(args: Array<string>): BmiValues {
-    if (args.length < 4) throw new Error('Not enough arguments');
-    if (args.length > 4) throw new Error('Too many arguments');
+// function parseArguments(args: Array<string>): BmiValues {
+//     if (args.length < 4) throw new Error('Not enough arguments');
+//     if (args.length > 4) throw new Error('Too many arguments');
 
-    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
-        return {
-            height: Number(args[2]),
-            weight: Number(args[3])
-        }
-    } else {
-        throw new Error('Provided values were not numbers!');
-    }
-}
+//     if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+//         return {
+//             height: Number(args[2]),
+//             weight: Number(args[3])
+//         }
+//     } else {
+//         throw new Error('Provided values were not numbers!');
+//     }
+// }
 
 function calculcateBMI(height: number, weight: number): string {
     const bmi: number = weight / (height * height) * 10000;
@@ -31,9 +31,11 @@ function calculcateBMI(height: number, weight: number): string {
     }
 }
 
-try {
-    const { height, weight } = parseArguments(process.argv);
-    console.log(calculcateBMI(height, weight));
-} catch (error) {
-    console.log(error.message);
-}
+export default calculcateBMI;
+
+// try {
+//     const { height, weight } = parseArguments(process.argv);
+//     console.log(calculcateBMI(height, weight));
+// } catch (error) {
+//     console.log(error.message);
+// }
--- part9/typescript-app/index.ts
+++ part9/typescript-app/index.ts
@@ -1,9 +1,33 @@
 import express from 'express';
+import calculcateBMI from './bmiCalculator';
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 });
+
+app.get('/bmi', (req, res) => {
+  if (!req.query.height || !req.query.weight) {
+    return res.status(400).json({
+      error: 'missing height or weight params'
+    });
+  }
+
+  const weight = Number(req.query.weight);
+  const height = Number(req.query.height);
+
+  if (isNaN(weight) || isNaN(height)) {
+    return res.status(400).json({
+      error: 'height and weight must be numbers'
+    });
+  }
+
+  return res.json({
+    weight,
+    height,
+    bmi: calculcateBMI(height, weight)
+  });
+})
 
 const PORT = 3001;
 
